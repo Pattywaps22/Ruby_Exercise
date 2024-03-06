8240628252a328809fa143a06533ba61c42d4f2e
ruby_09 <ruby_09@swzs-iMac.lan>
2024-03-06 11:01:49 +0800

    answer

--- Simple_Ruby/exercise_9.rb
+++ Simple_Ruby/exercise_9.rb
@@ -1,13 +1,13 @@
 #Write a Ruby program that takes a year as input and checks whether it is a leap year. Print the result.
 
 def calculate_leap_year(year)
-  if year % 400 == 0
+  if (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)
     puts "#{year} is a leap year"
   else
     puts "#{year} is not a leap year"
   end
 end
 
-year = 2024
+year = gets.chomp.to_i
 
 calculate_leap_year(year)
