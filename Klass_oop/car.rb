3ed92ea395d8b38ed25b4d13cac86925fca7429d
ruby_09 <ruby_09@swzs-iMac.lan>
2024-03-19 16:43:36 +0800

    unfinished

---
+++ Klass_oop/car.rb
@@ -0,0 +1,89 @@
+class Car
+    attr_accessor :color, :model, :year, :plate_number, :klass, :car_type
+
+    def initialize(color, model, year, plate_number, klass)
+      @speed = 0
+      @color = color
+      @model = model
+      @year = year
+      @plate_number = plate_number
+      @car_type = klass
+    end
+
+    def details
+      {
+        color: color,
+        model: model,
+        year: year,
+        plate_number: plate_number,
+        car_type: klass
+      }
+    end
+
+    def speed
+      @speed
+    end
+
+    def random_speed
+      @speed = rand(20)
+    end
+
+    def random_color
+      @color = rand(@color)
+    end
+
+    def increase_speed
+      @speed += 1
+    end
+
+    def decrease_speed
+      @speed -= 1 if @speed > 0
+    end
+  end
+cars = [
+  Car.new('red', 'toyota', '1997', 'E23 MAK', '1'),
+  Car.new('green', 'bmw', '2000','B24 MAK', '1'),
+  Car.new('blue', 'ford', '2001','D23 MAK', '3'),
+  Car.new('yellow', 'honda', '1998','F23 MAK', '2'),
+  Car.new('black', 'isuzu', '1996','MAK 1A', '2'),
+  Car.new('violet', 'hyundai', '2004','MA13 KER', '3'),
+  Car.new('white', 'jeep', '2020','MA10 KER', '1'),
+  Car.new('pink', 'kia', '1992','B10 MAK', '3'),
+  Car.new('gray', 'tesla', '2008','K21 MAK', '1'),
+  Car.new('orange', 'subaru', '2005','FA73 MAK', '2')
+]
+
+  # 3. Return most common color
+
+  # 1. filter the array of car class and return cars with class type 1
+# class_type_1 = cars.select { |car| car.car_type == '1' }
+# puts "Class Type 1 Cars:"
+# class_type_1.map { |car| puts car.details[:model] }
+
+  # 2. Print plate_number
+# puts "Cars plate number:"
+# cars.map {|car| puts "#{car.details[:model]} == #{car.details[:plate_number]}"}
+
+  # 3. Return most common color
+# most_common_color = cars.max { |car| car.color}
+# puts "most_common_color #{most_common_color}"
+# most_common_color.max { |car| puts cars.details[:color]}
+
+  #4. modify the decrease speed to avoid negative number
+# cars[0].increase_speed
+# cars[0].increase_speed
+# cars[0].decrease_speed
+# cars[0].decrease_speed
+# cars[0].increase_speed
+# cars[0].decrease_speed
+# puts "car speed = #{cars[0].speed}"
+
+  # 5. create a script that will change the speed of the class (make it random)
+# random = cars.each { |car| car.random_speed }
+# puts "random car speed:"
+# random.each { |car| puts "#{car.details[:model]} == speed (#{car.speed})" }
+
+  # 6. Modify the color of each car randomly (loop)
+  random = cars.each { |car| car.random_color }
+  puts "random car speed:"
+  random.each { |car| puts "#{car.details[:model]} == color (#{car.random_color})" }
